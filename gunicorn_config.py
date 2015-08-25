10a2e8105a307bdf733bcbc9ff6c41b01b6b2227
alex <AlexV348@gmail.com>
2015-08-25 16:14:05 -0700

    updated gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,4 @@
 command = '/home/alex/verify_project/vrfy_env/bin/gunicorn'
 pythonpath = '/home/alex/verify_project/vrfy'
-bind= '127.0.0.1:8000'
+bind= '127.0.0.1:8001'
 workers = 3
-user = 'nobody'
--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ VENV_DIR=$BASE_DIR/../vrfy_env
 
 source $VENV_DIR/bin/activate
 
-$VENV_DIR/bin/gunicorn vrfy.wsgi:application
+$VENV_DIR/bin/gunicorn -c /home/alex/verify_project/vrfy/gunicorn_config.py vrfy.wsgi
